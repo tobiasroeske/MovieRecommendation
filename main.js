7171f7fcc136d05b50d69d474f71b173c693d1fe
Tobias Roeske <t.roeske91@gmail.com>
2023-11-24 12:56:46 +0100

    Added functionality to main.js

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ async function getGenres() {
 // Create the option Elements for the Selector Element
 const createOptionElements = async (genreArray) => {
     const genreSelector = document.getElementById('genreSelector');
-    const genres =  await genreArray.map((item) => {
+    const genre =  await genreArray.map((item) => {
         let genreItem = document.createElement('option');
         genreItem.setAttribute = ('id', 'genreOption');
         genreItem.setAttribute = ('value', item);
@@ -33,9 +33,95 @@ const createOptionElements = async (genreArray) => {
         genreSelector.appendChild(genreItem);
 
     })
-    return genres;
+    return genre;
+}
+
+// Get a random Movie depending on Genre
+
+const randomMovieByGenre = async () => {
+    const url = 'https://imdb8.p.rapidapi.com/title/v2/get-popular-movies-by-genre';
+    const genreSelector = document.getElementById('genreSelector');
+    const genre = genreSelector.ariaValueMax;
+    const requiredParams = `?genre=${genre}&limit=100`;
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'e8ccc402c3msh3e9dc3e37473cbfp1c12c2jsn6b67c2c0e67c',
+		'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(`${url}${requiredParams}`, options);
+	if (response.ok) {
+        const jsonResponse = await response.json();
+        const randomMovieIndex = Math.floor(Math.random() * jsonResponse.length);
+        const randomMovie = jsonResponse[randomMovieIndex];
+        const randomMovieId = randomMovie.slice(7, randomMovie.length -1);
+        console.log(randomMovieId);
+        return randomMovieId;
+    }
+} catch (error) {
+	console.error(error);
+}
+}
+
+// Get Movie title and poster url depending on Movie ID
+
+const getMovieDetails = async (randomMovieId) => {
+    const url = 'https://imdb8.p.rapidapi.com/title/get-details';
+    const requiredParams =`?tconst=${randomMovieId}`;
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'e8ccc402c3msh3e9dc3e37473cbfp1c12c2jsn6b67c2c0e67c',
+		'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(`${url}${requiredParams}`, options);
+	if (response.ok) {
+        const jsonResponse = await response.json();
+        const movieTitle = jsonResponse.title;
+        console.log(movieTitle);
+        const moviePosterUrl = jsonResponse.image.url;
+        console.log(moviePosterUrl);
+        return randomMovieId;
+    }
+} catch (error) {
+	console.error(error);
+}
+}
+
+// get Plot desription based on movie Id
+
+const getMoviePlot = async(randomMovieId) => {
+    const url = 'https://imdb8.p.rapidapi.com/title/get-plots';
+    const requiredParams =`?tconst=${randomMovieId}`;
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'e8ccc402c3msh3e9dc3e37473cbfp1c12c2jsn6b67c2c0e67c',
+		'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(`${url}${requiredParams}`, options);
+	if (response.ok) {
+        const jsonResponse = await response.json();
+        const plotDescription = jsonResponse.plots[0].text;
+        console.log(plotDescription);
+    }
+} catch (error) {
+	console.error(error);
+}
 }
 
 
 getGenres().then(createOptionElements);
+randomMovieByGenre().then(getMovieDetails)
+.then(getMoviePlot);
+
 
